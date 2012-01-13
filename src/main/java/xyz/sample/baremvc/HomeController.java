357ddef8b207139bacfd76cc6c95c7dd42254cc0
Ustun Ozgur <ustunozgur@gmail.com>
2012-01-13 17:30:51 +0200

    Add request param example

--- src/main/java/xyz/sample/baremvc/HomeController.java
+++ src/main/java/xyz/sample/baremvc/HomeController.java
@@ -1,17 +1,27 @@
 package xyz.sample.baremvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
-	
+
 	@RequestMapping(value="/")
-	public String home() {
-			
-		System.out.println("Homecontroller:passing through");
-		return "home2";
-	}
+		public String home() {
+
+			System.out.println("Homecontroller:passing through");
+			return "home2";
+		}
+
+	@RequestMapping(value="/userInput") 
+		public String compare(@RequestParam("input1") String input1, Model model) {
+			String output = "The number you have " + input1;
+			model.addAttribute("output", output);
+			return "compareResult";
+		}	
+
 }	
-	
+
 
