e1ce235816153d86ab78e5b00d3050c2b5b81693
Ustun Ozgur <ustunozgur@gmail.com>
2012-01-13 17:04:31 +0200

    Initial commit

---
+++ src/main/java/xyz/sample/baremvc/HomeController.java
@@ -0,0 +1,17 @@
+package xyz.sample.baremvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value="/")
+	public String home() {
+			
+		System.out.println("Homecontroller:passing through");
+		return "/WEB-INF/views/home.jsp";
+	}
+}	
+	
+
